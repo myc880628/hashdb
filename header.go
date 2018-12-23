0c0195312b5dc6fc09d67211d3678bea46495d09
myc880628 <528290772@qq.com>
2018-12-23 19:54:41 +0800

    Create header.go

---
+++ header.go
@@ -0,0 +1,26 @@
+package hashdb
+
+import (
+	"encoding/binary"
+	"io"
+)
+
+type (
+	header struct {
+		flags uint16
+		ksize uint16
+		vsize uint32
+	}
+)
+
+func (h *header) encode(writer io.Writer) {
+	binary.Write(writer, binary.BigEndian, h.flags)
+	binary.Write(writer, binary.BigEndian, h.ksize)
+	binary.Write(writer, binary.BigEndian, h.vsize)
+}
+
+func (h *header) decode(reader io.Reader) {
+	binary.Read(reader, binary.BigEndian, &h.flags)
+	binary.Read(reader, binary.BigEndian, &h.ksize)
+	binary.Read(reader, binary.BigEndian, &h.vsize)
+}
