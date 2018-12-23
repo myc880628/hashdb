49bdf5bbf05294e707ee6e90d47a7f883c981fa5
myc880628 <528290772@qq.com>
2018-12-23 19:55:32 +0800

    Create trailer.go

---
+++ trailer.go
@@ -0,0 +1,20 @@
+package hashdb
+
+import (
+	"encoding/binary"
+	"io"
+)
+
+type (
+	trailer struct {
+		sum32 uint32
+	}
+)
+
+func (t *trailer) encode(writer io.Writer) {
+	binary.Write(writer, binary.BigEndian, t.sum32)
+}
+
+func (t *trailer) decode(reader io.Reader) {
+	binary.Read(reader, binary.BigEndian, &t.sum32)
+}
